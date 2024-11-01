312dbd6a8245d53c194efc115e013bad2f7641a0
miroks404 <themiroks@gmail.com>
2024-11-01 18:04:03 +0200

    KTB-02-01 corrections

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,12 +5,6 @@ import java.io.File
 fun main() {
 
     val wordsFile: File = File("words.txt")
-    wordsFile.createNewFile()
-    wordsFile.writeText("hello привет")
-    wordsFile.appendText("\ndog собака")
-    wordsFile.appendText("\ncat кошка")
 
-    for (i in wordsFile.readLines()) {
-        println(i)
-    }
+    wordsFile.forEachLine { println(it) }
 }
