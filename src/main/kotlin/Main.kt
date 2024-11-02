d2d335865cf19dec391d0505a9c13442a1ceae56
miroks404 <themiroks@gmail.com>
2024-11-02 11:42:21 +0200

    KTB-04 parsing lines in file

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,9 +2,27 @@ package org.example
 
 import java.io.File
 
+data class Word(
+    val original: String,
+    val translation: String,
+    val correctAnswersCount: Int,
+)
+
 fun main() {
 
     val wordsFile: File = File("words.txt")
 
-    wordsFile.forEachLine { println(it) }
+    val dictionary: MutableList<Word> = mutableListOf()
+
+    wordsFile.forEachLine {
+        val listOfSplitWords = it.split("|")
+        val word = Word(
+            listOfSplitWords[0],
+            listOfSplitWords[1],
+            if (listOfSplitWords[2] == "") 0 else listOfSplitWords[2].toInt()
+        )
+        dictionary.add(word)
+    }
+
+    println(dictionary)
 }
