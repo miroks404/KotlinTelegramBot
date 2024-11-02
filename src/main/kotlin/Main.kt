6971d8f6ea9ed9f23ee4225ce9cad02326168efa
miroks404 <themiroks@gmail.com>
2024-11-02 11:36:05 +0200

    Revert "KTB-04 parsing lines in file"
    This reverts commit f29e4c5d35cf5228faab61c22f6499b695c482e4.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,27 +2,9 @@ package org.example
 
 import java.io.File
 
-data class Word(
-    val original: String,
-    val translate: String,
-    val correctAnswersCount: Int = 0,
-)
-
 fun main() {
 
     val wordsFile: File = File("words.txt")
 
-    val dictionary: MutableList<Word> = mutableListOf()
-
-    wordsFile.forEachLine {
-        val splitListOfWords = it.split("|")
-        val word = Word(
-            splitListOfWords[0],
-            splitListOfWords[1],
-            if (splitListOfWords[2] == "") 0 else splitListOfWords[2].toInt()
-        )
-        dictionary.add(word)
-    }
-
-    println(dictionary)
+    wordsFile.forEachLine { println(it) }
 }
