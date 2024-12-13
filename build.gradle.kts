b1f9882cf3e3e0f14716f12e437431e7c59953d5
miroks404 <themiroks@gmail.com>
2024-12-13 22:30:01 +0200

    KTB public

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     kotlin("jvm") version "2.1.0"
     kotlin("plugin.serialization") version "2.1.0"
+    id("com.github.johnrengelman.shadow") version "7.1.2"
+    application
 }
 
 group = "org.example"
@@ -20,4 +22,8 @@ tasks.test {
 }
 kotlin {
     jvmToolchain(21)
+}
+
+application {
+    mainClass.set("TelegramKt")
 }
